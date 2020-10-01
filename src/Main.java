0ce1cb62c7a97c223eb1ba0df2a3eef9bb7c94cd
uvsq21807159 <kenza.hadj-mahfoud@ens.uvsq.fr>
2020-10-01 18:56:52 +0200

    modification de Main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		
+		Fraction f = new Fraction(25,6);
+	    System.out.println(f.toString());
+	   
 		
 		
 
