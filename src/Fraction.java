01dc7079c295b085d3305ca670e497087182c1fc
uvsq21807159 <kenza.hadj-mahfoud@ens.uvsq.fr>
2020-10-01 18:57:24 +0200

    ajout des modificarion de la classe fraction

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,4 +1,20 @@
 
 public class Fraction {
+	private int numerateur;
+	private int denominateur;
+	
+	
+	Fraction(int num,int denom)
+	{
+		this.numerateur = num;
+		this.denominateur = denom;
+	}
+	
+	public String toString()
+	{
+		String str = this.numerateur + "et " + this.denominateur;
+		return str;
+	}
+
 
 }
